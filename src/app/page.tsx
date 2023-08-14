12db532dd2f30cc8160c9a1505dc597a295f54e7
Martin <25070051+anquetil@users.noreply.github.com>
2023-08-14 16:32:10 +0200

    better formatting + image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,6 @@ import { MintButton } from '@/components/MintButton';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import { useAccount, useNetwork, useSwitchNetwork } from 'wagmi';
 
-// zora testnet 999
-// zora 7777777
-
 const chainInfo:Array<{name: string, id: number}> = [
    {
       name: "Zora Testnet",
@@ -29,22 +26,21 @@ export default function Home() {
    const { chains, isLoading: isLoadingNetworkSwitch, pendingChainId, switchNetwork } = useSwitchNetwork()
    const correctChain = chain?.id === intendedChain.id
 
+   
    return (
-      <main className="flex min-h-screen flex-col items-center pt-12 px-6 sm:pt-24 sm:px-0  bg-gray-50">
-         <div className="text-4xl font-bold mb-8">FORTUNE COOKIE</div>
-         <div className="text-xl text-gray-600 mb-8">Mint a free, random Zora NFT.</div>
+      <main className="flex min-h-screen flex-col items-center pt-12 px-6 sm:pt-24 sm:px-0  bg-[fcfcfc]">
+         <div className="text-3xl font-bold mb-8">MINT A FREE RANDOM ZORA NFT</div>
          {
             (!isConnected) 
             ?
             (
                <ConnectButton showBalance={false} accountStatus="avatar" />
-               /*<div className="bg-yellow-100 text-yellow-600 rounded-sm ring-yellow-400 ring-1 px-4 py-3">Connect to mint</div> */
             )
             :
                (correctChain && address != undefined) 
                ?
                (
-                  <MintButton userAddress={address} chainId={999} />
+                  <MintButton userAddress={address} chain={chain} />
                )
                :
                (
@@ -53,7 +49,7 @@ export default function Home() {
                      onClick={() => switchNetwork?.(intendedChain.id)}
                   >
                      <div className="text-opacity-75 text-black">
-                        Connect to {intendedChain.name}
+                        CONNECT TO {intendedChain.name.toUpperCase()}
                      </div>
                   </button>
                )
--- src/components/MintButton.tsx
+++ src/components/MintButton.tsx
@@ -3,16 +3,16 @@
 import { decodeFunctionData, parseEther } from 'viem/utils';
 import { erc721DropABI } from "@zoralabs/zora-721-contracts";
 import { Address } from 'viem';
-import { useContractWrite, useWaitForTransaction, usePrepareContractWrite, useContractRead } from 'wagmi';
+import { useContractWrite, useWaitForTransaction, usePrepareContractWrite, useContractRead, Chain } from 'wagmi';
 import { getContracts } from '@/utils/getContracts';
+import { LoadingNoggles } from './LoadingNoggles';
+import Image  from 'next/image'
 const referralAddress = '0x6ab075abfA7cdD7B19FA83663b1f2a83e4A957e3'
 var sample = require('lodash/sample');
 
-export function MintButton({ userAddress, chainId }: { userAddress: Address, chainId: number }) {
+export function MintButton({ userAddress, chain }: { userAddress: Address, chain: Chain }) {
    const possibleContracts = getContracts()
    const NFT721Contract = sample(possibleContracts) as Address //random contract
-   console.log('possible: ', possibleContracts, 'chosen: ', NFT721Contract)
-
    const { data } = useContractRead({
       address: NFT721Contract,
       abi: erc721DropABI,
@@ -21,11 +21,8 @@ export function MintButton({ userAddress, chainId }: { userAddress: Address, cha
 
    const json = atob(data!.substring(29));
    const result = JSON.parse(json);
-   console.log(result);
-
 
    const { config: prepareConfig, error: prepareError } = usePrepareContractWrite({
-      //chainId: chainId,
       address: NFT721Contract,
       abi: erc721DropABI,
       functionName: 'mintWithRewards',
@@ -40,8 +37,15 @@ export function MintButton({ userAddress, chainId }: { userAddress: Address, cha
       hash: writeData?.hash,
    })
 
+   const txnHash = transactionData?.transactionHash
+   const imageData = result.image as string
+   const imageLink = imageData.includes("ipfs://") ? 
+      `https://ipfs.io/ipfs/${imageData.substring(7)}`
+      :
+      imageData
+
    return(
-      <div>
+      <div className="items-center flex flex-col">
          <button
             className="border-gray-300 border-[1px] px-5 py-4 rounded bg-white shadow text-xl hover:bg-gray-50 active:shadow-sm active:mt-[2px]"
             disabled={!write}
@@ -50,8 +54,26 @@ export function MintButton({ userAddress, chainId }: { userAddress: Address, cha
             I'M FEELING LUCKY
          </button>
 
-         {isLoading && <div>Minting in progress</div>}
-         {isSuccess && <div>Completed! Transaction: {transactionData?.transactionHash}</div>}
+         {isLoading && 
+            <div className="mt-5">
+            <LoadingNoggles/>
+               <div className="text-center">Minting in progress</div>
+            </div>}
+
+         {isSuccess && 
+            <div className="mt-5 text-center flex flex-col items-center">
+               <Image
+                  src={imageLink}
+                  width={150}
+                  height={150}
+                  alt="your NFT" />
+               <div>Minted!</div>
+               <br></br>
+               <a className="text-blue-700 underline" href={`${chain.blockExplorers?.default.url}/tx/${txnHash}`}>
+                  {`${txnHash?.substring(0,6)}...${txnHash?.slice(-6)}`}
+               </a>
+            </div>
+         }
 
       </div>
 
